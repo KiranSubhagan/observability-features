93329c04b6f924ec6ff112f9653b89ad99616abc
KiranSubhagan <subhagankiran21@gmail.com>
2026-05-12 19:12:24 +0100

    Update outputs.tf

--- MonitorExport/Terraform/outputs.tf
+++ MonitorExport/Terraform/outputs.tf
@@ -1,15 +1,8 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
+
  
-variable "lambda_name" {
-  default = "monitor-export"
-}
+
 
 output "lambda_arn" {
   value = aws_lambda_function.monitor_export.arn
 }
  
-variable "sender_email" {}
- 
-variable "receiver_email" {}
