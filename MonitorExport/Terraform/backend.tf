c2b7ea099da72a581239db5fad23511f946e13e9
KiranSubhagan <subhagankiran21@gmail.com>
2026-05-12 19:42:22 +0100

    Update backend.tf

--- MonitorExport/Terraform/backend.tf
+++ MonitorExport/Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ecs-demo-terraform-state"
-    key            = "datadog/dev/terraform.tfstate"
+    key            = "datadog/dev/terraform_monitor_export.tfstate"
     region         = "ap-south-1"
   }
 }
